986d67e144ec541aa13b9de2ef16a88ae515576c
marianogambino <marianogambino@e62c76c0-1556-0410-9b61-773e0b53d292>
2008-09-05 02:33:19 +0000

    Eliminacion src

--- trunk/src/clasesVarias/KorFactory.java
+++
@@ -1,9 +0,0 @@
-package clasesVarias;
-
-public class KorFactory 
-{
-	public static KorServices getInstance()
-	{
-		return new KorServices();
-	}
-}
--- trunk/src/clasesVarias/ValoresItf.java
+++
@@ -1,7 +0,0 @@
-package clasesVarias;
-
-public interface ValoresItf 
-{
-	public String getValor();
-	public String getDescripcion();
-}
